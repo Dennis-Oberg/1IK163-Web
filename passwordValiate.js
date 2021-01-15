5020770a51b3801671190f1d6fd942adc67eb337
Dennis-Oberg <60152405+Dennis-Oberg@users.noreply.github.com>
2021-01-15 16:44:42 +0100

    lagt till en javascript fil samt ändrat så jag(Dennis) har en egen sida samt att gustaf (gustaf.html) har en egen sida

---
+++ passwordValiate.js
@@ -0,0 +1,42 @@
+"use strict";
+
+let userName = ["Dennis", "Gustaf"];
+let password = ["Admin123", "123", "asd"];
+let form = document.createElement("form");
+let input = document.createElement("input");
+
+function init() {
+    validateLogin();
+    validatePassword();
+
+}
+
+
+function validatePassword() {
+    let passinput = input.value;
+
+    for (var i = 0; i < password.length; i++) {
+        if (passinput == password[i]) {
+            return true;
+        }
+        if (passinput != password[i]) {
+            return false;
+        }
+    }
+}
+
+
+function validateLogin() {
+    let formInput = input.value;
+
+
+    for (var i = 0; i < userName.length; i++) {
+        if (formInput == userName[i]) {
+            return true;
+        }
+        if (formInput != userName) {
+            return false;
+        }
+    }
+
+}
